39f48bf66f31ba39b86bc393abdfc1c627cdddc7
vipul-hedaoo <78016931+vipul-hedaoo@users.noreply.github.com>
2024-03-08 01:29:41 +0000

    iife

--- practice/imeditelyInvokeFunction.js
+++ practice/imeditelyInvokeFunction.js
@@ -1,9 +1,13 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+// if will used to immeditely invoke the function and to reduce the polution of the global scope....
 
 (function chai(){
-    // named IIFE
+    // named IIFE=Imeditely Invoke function expression
     console.log(`DB CONNECTED`);
+})();
+
+(function hello(name){
+    console.log(`welcome ${name}`);
 })();
 
 ( (name) => {
